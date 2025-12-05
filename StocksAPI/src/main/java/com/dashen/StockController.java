17691d976eccabd950677bd49b17bb0ddafc9bc8
Guru <gurumurthy.ramamurthy@gmail.com>
2025-12-05 19:28:00 +0530

    Attempt 1 - cicd process

--- StocksAPI/src/main/java/com/dashen/StockController.java
+++ StocksAPI/src/main/java/com/dashen/StockController.java
@@ -9,6 +9,6 @@ public class StockController {
 
 	@RequestMapping(value="/stocks", method=RequestMethod.GET)
 	public String m1() {
-		return "A list of stocks";
+		return "A list of stocks - attempt1";
 	}
 }
